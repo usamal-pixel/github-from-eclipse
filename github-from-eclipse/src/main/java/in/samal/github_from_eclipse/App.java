ade9b71a9d1e75738da6d12e7b86e2165376f7c5
usamal-pixel <67044654+usamal-pixel@users.noreply.github.com>
2020-07-08 13:03:42 +0530

    added by 2nd developer

--- github-from-eclipse/src/main/java/in/samal/github_from_eclipse/App.java
+++ github-from-eclipse/src/main/java/in/samal/github_from_eclipse/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+         System.out.println( "change done by John Mathur other developer" );
         System.out.println( "Hello World!"+"First change from java side...." );
     }
 }

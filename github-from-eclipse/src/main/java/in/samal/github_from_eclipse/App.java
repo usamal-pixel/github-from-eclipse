428cb4b629e8b42dd882e209c53803f27b5e1da9
usamal-pixel <utkalsamal8@gmail.com>
2020-07-08 12:59:01 +0530

    firstcommit

---
+++ github-from-eclipse/src/main/java/in/samal/github_from_eclipse/App.java
@@ -0,0 +1,13 @@
+package in.samal.github_from_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
